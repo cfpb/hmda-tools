8c10a8bb9bd516f99c4d794fa6b5fc679951d7da
Clinton N. Dreisbach <crnixon@gmail.com>
2013-02-10 09:37:34 -0500

    Added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hmda_tools',
+      version='0.1',
+      description='Tools to make working with HMDA data easier.',
+      url='http://github.com/crnixon/hmda_tools',
+      author='Clinton Dreisbach and others',
+      author_email='clinton@dreisbach.us',
+      license='Public domain',
+      packages=['hmda_tools'],
+      zip_safe=False)
